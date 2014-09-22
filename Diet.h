a489b53f92ce8bfdf31c4cf0c525904bdb52ccbb
mriddi <mriddi@bk.ru>
2014-09-22 23:14:00 +0400

    before roll back

---
+++ Diet.h
@@ -0,0 +1,38 @@
+//
+//  Diet.h
+//  Diet
+//
+//  Created by mriddi on 22.09.14.
+//  Copyright (c) 2014 Crocodev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class WeightHistory;
+
+@interface Diet : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * aimWeight;
+@property (nonatomic, retain) NSNumber * currentWeight;
+@property (nonatomic, retain) NSNumber * dayPoints;
+@property (nonatomic, retain) NSNumber * restDayPoints;
+@property (nonatomic, retain) NSDate * startDate;
+@property (nonatomic, retain) NSNumber * startWeight;
+@property (nonatomic, retain) NSSet *toPointsHistory;
+@property (nonatomic, retain) NSSet *toWeightHistory;
+@end
+
+@interface Diet (CoreDataGeneratedAccessors)
+
+- (void)addToPointsHistoryObject:(NSManagedObject *)value;
+- (void)removeToPointsHistoryObject:(NSManagedObject *)value;
+- (void)addToPointsHistory:(NSSet *)values;
+- (void)removeToPointsHistory:(NSSet *)values;
+
+- (void)addToWeightHistoryObject:(WeightHistory *)value;
+- (void)removeToWeightHistoryObject:(WeightHistory *)value;
+- (void)addToWeightHistory:(NSSet *)values;
+- (void)removeToWeightHistory:(NSSet *)values;
+
+@end
--- InputViewController.h
+++ InputViewController.h
@@ -13,9 +13,10 @@
 #define alphaMin 0.4
 #define dBetweenImages 10.0
 
-@interface InputViewController : UIViewController <ZenKeyboardDelegate, UITableViewDataSource, UITableViewDelegate>
+@interface InputViewController : UIViewController <ZenKeyboardDelegate, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 
 @property (assign, nonatomic) CGFloat alphaStep;
+@property (strong, nonatomic) UISearchBar *searchBar;
 @property (strong, nonatomic) UIImageView *foodView;
 @property (strong, nonatomic) UIImageView *weightView;
 @property (strong, nonatomic) ZenKeyboard * keyboard;
